3807be8b1215b3ef764a774604ca20496019fcc8
eliottgrougi <geliott0406@gmail.com>
2026-01-12 12:02:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 #main(code principal)
 
 texte_bienevnue = "Bienvenue chez Lycée & So, le site qui rend l'analyse des lycées beaucoup plus simple !"
+
+def search_number(val,v):
+    gauche = 0
+    droite = len(v) - 1
+    milieu = (gauche + droite) //2
+    if val == v[milieu]:
+        return milieu
+    elif val < v[milieu]:
+        droite = milieu - 1
+    else:
+        gauche = milieu + 1
+    return milieu
