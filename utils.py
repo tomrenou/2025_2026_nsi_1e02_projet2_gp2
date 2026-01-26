8847cda875f1ba6d700e33d68cce971bbf2b4723
eliottgrougi <geliott0406@gmail.com>
2026-01-26 12:54:34 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,5 +2,11 @@ import pandas as pd
 
 def make_data_frame_from_csv(csv_file):
     df = pd.read_csv('data.csv') 
-    return df 
-def get_departement()
+    return df
+
+def get_code_departement(lycees,Nom de l'etablissment):
+    ...
+    return Code du departement
+
+
+                    
