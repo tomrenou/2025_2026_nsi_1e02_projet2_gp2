b4cf115a55c74669c32fa4cb7214aefc67d1e332
Tom <tom.renou2009@icloud.com>
2026-01-26 13:00:41 +0100

    wip get etc

--- utils.py
+++ utils.py
@@ -2,18 +2,16 @@ import pandas as pd
 
 def make_data_frame_from_csv(csv_file):
     df = pd.read_csv('data.csv') 
-<<<<<<< HEAD
     return df
 
 def get_code_departement(lycees,Nom de l'etablissment):
     ...
     return Code du departement
 
+def get_académie(lycees,Nom de l'etablissment):
+    ...
+    return académie
 
-                    
-=======
-    return df 
-def get_departement():
-
-    
->>>>>>> bee1575026ed7a462eb0d0173637546c3334d3f6
+def get_année_scolaire(lycees,Nom de l'etablissment):
+    ...
+    return Code du departement
