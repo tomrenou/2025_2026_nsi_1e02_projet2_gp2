68e4ecad8b710579a6befbf93d67d5ea5fc490ef
eliottgrougi <geliott0406@gmail.com>
2026-01-26 11:30:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ texte_bienevnue = "Bienvenue chez Lycée & So, le site qui rend l'analyse des ly
 def search_number(val,v):
     gauche = 0
     droite = len(v) - 1
-    milieu = (gauche + droite) //2
-    if val == v[milieu]:
-        return milieu
-    elif val < v[milieu]:
-        droite = milieu - 1
-    else:
-        gauche = milieu + 1
+    while gauche <= droite:
+        milieu = (gauche + droite) //2
+        if val == v[milieu]:
+            return milieu
+        elif val < v[milieu]:
+            droite = milieu - 1
+        else:
+            gauche = milieu + 1
     return milieu
