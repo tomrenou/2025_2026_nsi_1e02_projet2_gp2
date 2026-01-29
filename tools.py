7210fd863e6e7be08bdadf988b4e1ebac209ebd3
eliottgrougi <geliott0406@gmail.com>
2026-01-29 11:43:12 +0100

    WIP

--- tools.py
+++ tools.py
@@ -1,3 +1,8 @@
 #tools
+import pandas as pd 
+import numpy as np 
+print("Pandas version :", pd.__version__) 
+print("Numpy version :", np.__version__)
+
 data_lycee.to_csv("cleaned_data_lycee.csv") 
 
