0617a06f1f1eb0a3d6f880c07d034d7b26dff845
eliottgrougi <geliott0406@gmail.com>
2026-02-09 12:08:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,6 @@
 
 texte_bienevnue = "Bienvenue chez Lycée & So, le site qui rend l'analyse des lycées beaucoup plus simple !"
 
-def search_number(val,v):
-    gauche = 0
-    droite = len(v) - 1
-    while gauche <= droite:
-        milieu = (gauche + droite) //2
-        if val == v[milieu]:
-            return milieu
-        elif val < v[milieu]:
-            droite = milieu - 1
-        else:
-            gauche = milieu + 1
-    return milieu
-
 import csv
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
